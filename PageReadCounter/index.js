e99ef738e32167fcdb5d508872d8ccc21dd4d55d
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-06-12 14:12:35 -0300

    Atualização

--- PageReadCounter/index.js
+++ PageReadCounter/index.js
@@ -1,1 +1,10 @@
-var button = document.querySelector("button");
+var buttonElement = document.querySelector("button");
+var counterElement = document.querySelector(".counter")
+var counter = 0;
+
+function count() {
+    counter++;
+    counterElement.after(counter.toString());
+}
+
+button.onclick = count;
